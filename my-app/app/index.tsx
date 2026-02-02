4a32289769fcec4c7d16b9c1be1f32949646cfc2
LtaiefAtef <ltaiefatef123@gmail.com>
2026-02-02 16:26:58 +0100

    My App Project Setup & Overview

---
+++ my-app/app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={{height:"100%",display:"flex",alignItems:"center",justifyContent:"center"}}>
+      <Text style={{fontSize:20,margin:""}}>Hello</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
